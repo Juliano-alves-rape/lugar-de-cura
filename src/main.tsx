09dd1cc414fb20c1add86f7f07706dc41edf963b
Juliano-alves-rape <alvesjuliano888@gmail.com>
2026-03-17 14:44:01 -0300

    Integrate Facebook Pixel tracking in main.tsx
    Added PixelInit component to initialize Facebook Pixel tracking.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,32 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { useEffect } from "react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+function PixelInit() {
+  useEffect(() => {
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];
+    t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)
+    (window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1260016016228195');
+    fbq('track', 'PageView');
+  }, []);
+
+  return null;
+}
+
+createRoot(document.getElementById("root")!).render(
+  <>
+    <PixelInit />
+    <App />
+  </>
+);
